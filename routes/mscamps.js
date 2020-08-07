34b7e8b290cd50f6d1bfc35328f51d528d9225f0
jcq <jiangchun_qiao@163.com>
2020-08-07 20:59:25 +0800

    合并/:id路由下更新删除路由

--- routes/mscamps.js
+++ routes/mscamps.js
@@ -15,15 +15,15 @@ const {
 //访问/会走get 或者 post
 router.route("/").get(getMscamps).post(createMscamp);
 
-router.route("/:id").get(getMscamp);
+router.route("/:id").get(getMscamp).put(updateMscamp).delete(deleteMscamp);
 
 //创建数据 与get合并
 // router.route("/").post(createMscamp);
 
 //更新数据
-router.route("/:id").put(updateMscamp);
+// router.route("/:id").put(updateMscamp);
 
 //删除数据
-router.delete("/:id").delete(deleteMscamp);
+// router.delete("/:id").delete(deleteMscamp);
 
 module.exports = router;
