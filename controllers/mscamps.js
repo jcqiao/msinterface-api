613e3422c20147eaeb253011d71dddecf0f31a5e
jcq <jiangchun_qiao@163.com>
2020-08-07 20:57:24 +0800

    优化更新删除路由

--- controllers/mscamps.js
+++ controllers/mscamps.js
@@ -5,3 +5,24 @@ exports.getMscamps = (req, res, next) => {
 exports.createMscamp = (req, res, next) => {
   res.status(200).json({ success: true, msg: "创建米修数据" });
 };
+
+//获取一个
+exports.getMscamp = (req, res, next) => {
+  res
+    .status(200)
+    .json({ success: true, msg: `根据${req.params.id}获取单个米修数据` });
+};
+
+//更新数据
+exports.updateMscamp = (req, res, next) => {
+  res
+    .status(200)
+    .json({ success: true, msg: `根据${req.params.id}更新米修数据` });
+};
+
+//删除数据
+exports.deleteMscamp = (req, res, next) => {
+  res
+    .status(200)
+    .json({ success: true, msg: `根据${req.params.id}删除米修数据` });
+};
--- routes/mscamps.js
+++ routes/mscamps.js
@@ -2,34 +2,28 @@ const express = require("express");
 const router = express.Router();
 
 //引入控制器 controller
-const { getMscamps, createMscamp } = require("../controllers/mscamps.js");
+const {
+  getMscamps,
+  createMscamp,
+  getMscamp,
+  updateMscamp,
+  deleteMscamp,
+} = require("../controllers/mscamps.js");
 
 //http://localhost:5000/api/v1/mscamps
 //当访问根路径时 就跳转 因此get->route 使用get方法执行getMscamps
 //访问/会走get 或者 post
 router.route("/").get(getMscamps).post(createMscamp);
 
-router.get("/:id", (req, res) => {
-  res
-    .status(200)
-    .json({ success: true, msg: `根据${req.params.id}获取单个米修数据` });
-});
+router.route("/:id").get(getMscamp);
 
 //创建数据 与get合并
 // router.route("/").post(createMscamp);
 
 //更新数据
-router.put("/:id", (req, res) => {
-  res
-    .status(200)
-    .json({ success: true, msg: `根据${req.params.id}更新米修数据` });
-});
+router.route("/:id").put(updateMscamp);
 
 //删除数据
-router.delete("/:id", (req, res) => {
-  res
-    .status(200)
-    .json({ success: true, msg: `根据${req.params.id}删除米修数据` });
-});
+router.delete("/:id").delete(deleteMscamp);
 
 module.exports = router;
