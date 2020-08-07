48dfe50931e17bc72729ae54c766c9ac0a8a219f
jcq <jiangchun_qiao@163.com>
2020-08-07 11:19:54 +0800

    创建路由对象 将app改为路由对象 并导出

--- routes/mscamps.js
+++ routes/mscamps.js
@@ -1,35 +1,34 @@
-//创建路由 /: http://localhost:5000
-app.get("/", (req, res) => {
-  res.status(200).json({ success: true, msg: "hi jcq" });
-});
+const express = require("express");
+const router = express.Router();
 
 //http://localhost:5000/api/v1/mscamps
-app.get("/api/v1/mscamps", (req, res) => {
+router.get("/", (req, res) => {
   res.status(200).json({ success: true, msg: "获取所有米修数据" });
 });
 
-//http://localhost:5000/api/v1/mscamps/:id
-app.get("/api/v1/mscamps/:id", (req, res) => {
+router.get("/:id", (req, res) => {
   res
     .status(200)
     .json({ success: true, msg: `根据${req.params.id}获取单个米修数据` });
 });
 
 //创建数据
-app.post("/api/v1/mscamps", (req, res) => {
+router.post("/", (req, res) => {
   res.status(200).json({ success: true, msg: "创建米修数据" });
 });
 
 //更新数据
-app.put("/api/v1/mscamps/:id", (req, res) => {
+router.put("/:id", (req, res) => {
   res
     .status(200)
     .json({ success: true, msg: `根据${req.params.id}更新米修数据` });
 });
 
 //删除数据
-app.delete("/api/v1/mscamps/:id", (req, res) => {
+router.delete("/:id", (req, res) => {
   res
     .status(200)
     .json({ success: true, msg: `根据${req.params.id}删除米修数据` });
 });
+
+module.exports = router;
